83e071c4dad312b93fc6f079f6fb816b0cd9fa94
Michael Cao <caozibo2000@gmail.com>
2022-06-05 18:44:09 -0400

    working

--- Assets/Scripts/TerrainSystem/BrushSystem.cs
+++ Assets/Scripts/TerrainSystem/BrushSystem.cs
@@ -1,15 +1,82 @@
 using System;
+using System.Drawing.Drawing2D;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class BrushSystem
 {
-    public BrushSystem()
+    public enum BrushType
     {
-        // brush types and sizes
+        SmallSquare
+    }
+    
+    public struct IndexsAndChunk
+    {
+        public int2 coord;
+        public int3 local;
+        public IndexsAndChunk(int2 coord, int3 local)
+        {
+            this.coord = coord;
+            this.local = local;
+        }
     }
 
-    public void EvaluateBrush(Vector3 position)
+    public BrushType brushType;
+
+    public BrushSystem()
     {
+        // init brush system
+        brushType = BrushType.SmallSquare;
+    }
+
+    public IndexsAndChunk[] EvaluateBrush(Vector3 position)
+    {
+        switch (brushType)
+        {
+            case BrushType.SmallSquare:
+
+                IndexsAndChunk[] indexsAndChunksArray = new IndexsAndChunk[27];
+
+                position /= 15;
+                
+                int x = Mathf.RoundToInt(position.x);
+                int y = Mathf.RoundToInt(position.y);
+                int z = Mathf.RoundToInt(position.z);
+
+                int counter = 0;
+                
+                for (int i = -1; i < 2; i++)
+                {
+                    for (int j = -1; j < 2; j++)
+                    {
+                        for (int k = -1; k < 2; k++)
+                        {
+                            int _x = x + i;
+                            int _y = y + j;
+                            int _z = k + k;
+                            
+                            // find the coord
+                            // check if outside of bound
+                            // find local coord of index to the chunk
+                            // duplicate coord if necessary
+
+                            IndexsAndChunk indexsAndChunk = new IndexsAndChunk(
+                                new int2(-1, -1),
+                                new int3(-1, -1, -1)
+                                );
+                            indexsAndChunksArray[counter] = indexsAndChunk;
+                            counter++;
+                            // find all corresponding chunks and which set of x y z belongs to which chunk
+                        }
+                    }
+                }
+
+                return indexsAndChunksArray;
+                
+            default:
+                return new IndexsAndChunk[1];
+        }
         // find corresponding int2 and get chunk from dictionary
         // subtract position from chunk position
         // get local point indexes
@@ -19,7 +86,5 @@ public class BrushSystem
         
         // update data for each chunk
         // call compute shader to triangulate for each chunk
-
-        throw new NotImplementedException();
     }
 }
